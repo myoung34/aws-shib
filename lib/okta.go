65cdafd494b3eed88537590e8e048f236fbc43da
Jason Armbruster <Jason.Armbruster@colorado.edu>
2018-05-01 14:00:38 -0600

    Initial version - works with login command

--- lib/okta.go
+++ lib/okta.go
@@ -3,7 +3,8 @@ package lib
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
+	"encoding/xml"
+  "errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,7 +13,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
+  "encoding/base64"
 	"golang.org/x/net/publicsuffix"
 
 	"github.com/99designs/keyring"
@@ -21,10 +22,30 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/segmentio/aws-okta/lib/saml"
+
+	"github.com/anaskhan96/soup"
 )
 
 const (
 	OktaServer = "okta.com"
+  // region: The default AWS region that this script will connect
+  // to for all API calls
+  region = "us-west-2"
+
+  // output format: The AWS CLI output format that will be configured in the
+  // saml profile (affects subsequent CLI calls)
+  outputformat = "json"
+
+  // awsconfigfile: The file where this script will store the temp
+  // credentials under the saml profile
+  awsconfigfile = "/.aws/credentials"
+
+  // SSL certificate verification: Whether or not strict certificate
+  // verification is done, False should only be used for dev/test
+  sslverification = true
+
+  // idpentryurl: The initial url that starts the authentication process.
+  idpentryurl = "https://***REMOVED***/idp/profile/SAML2/Unsolicited/SSO?providerId=urn:amazon:webservices"
 )
 
 type OktaClient struct {
@@ -402,43 +423,98 @@ type OktaProvider struct {
 }
 
 func (p *OktaProvider) Retrieve() (sts.Credentials, string, error) {
-	log.Debug("using okta provider")
-	item, err := p.Keyring.Get("okta-creds")
+	log.Debug("using shib provider")
+	item, err := p.Keyring.Get("shib-creds")
 	if err != nil {
-		log.Debug("couldnt get okta creds from keyring: %s", err)
+		log.Debug("couldnt get shib creds from keyring: %s", err)
 		return sts.Credentials{}, "", err
 	}
 
 	var oktaCreds OktaCreds
 	if err = json.Unmarshal(item.Data, &oktaCreds); err != nil {
-		return sts.Credentials{}, "", errors.New("Failed to get okta credentials from your keyring.  Please make sure you have added okta credentials with `aws-okta add`")
+		return sts.Credentials{}, "", errors.New("Failed to get shib credentials from your keyring.  Please make sure you have added shib credentials with `aws-okta add`")
 	}
 
-	// Check for stored session cookie
-	var sessionCookie string
-	cookieItem, err := p.Keyring.Get("okta-session-cookie")
-	if err == nil {
-		sessionCookie = string(cookieItem.Data)
+	
+	//Start JMA Code here
+	resp, err := soup.Get(idpentryurl)
+	if err != nil {
+		return sts.Credentials{}, "", errors.New("Failed to access Shibboleth URL")
 	}
+	log.Debug("Step: 1")
+	doc := soup.HTMLParse(resp)
+	inputs := doc.FindAll("input")
+	payload := url.Values{}
+	for _, input := range inputs {
+		var name = input.Attrs()["name"]
+		var value = input.Attrs()["value"]
+		if (strings.Contains(strings.ToLower(name),"user")) {
+			payload.Add(name,oktaCreds.Username)
+		} else if (strings.Contains(strings.ToLower(name),"email")) {
+			payload.Add(name,oktaCreds.Username)
+		} else if (strings.Contains(strings.ToLower(name),"pass")) {
+			payload.Add(name,oktaCreds.Password)
+		} else if (strings.Contains(strings.ToLower(name),"revoke")) {
+			//fmt.Println("Not setting revoke attribute")
+		} else {
+			payload.Add(name, value)
+		}
+	}
+	payload.Add("_eventId_proceed", "")
 
-	oktaClient, err := NewOktaClient(oktaCreds, p.OktaAwsSAMLUrl, sessionCookie)
+	formaction := doc.Find("form").Attrs()["action"]
+	idpurl, err := url.Parse(idpentryurl)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var idpauthformsubmiturl = idpurl.Scheme + "://" + idpurl.Host + formaction
+
+	var resp2, _ = http.PostForm(idpauthformsubmiturl, payload)
+	defer resp2.Body.Close()
+	bytes, err := ioutil.ReadAll(resp2.Body)
+
+	//Duo challenge goes here
+  log.Debug("Step: 3")
+	var assertion = ""
+	doc = soup.HTMLParse(string(bytes))
+	inputs = doc.FindAll("input")
+	for _, input := range inputs {
+		var name = input.Attrs()["name"]
+		if (strings.Contains(name,"SAMLResponse")) {
+			assertion = input.Attrs()["value"]
+		}
+	}
+	if assertion == "" {
+		return sts.Credentials{}, "", errors.New("No SAML assertion in response")
+	}
+  var s saml.Response
+	var a, _ = base64.StdEncoding.DecodeString(assertion)
+  err = xml.Unmarshal(a, &s)
+
+	principal, role, err := GetRoleFromSAML(&s, p.ProfileARN)
 	if err != nil {
 		return sts.Credentials{}, "", err
 	}
 
-	creds, newSessionCookie, err := oktaClient.AuthenticateProfile(p.ProfileARN, p.SessionDuration)
+	// Step 4 : Assume Role with SAML
+  log.Debug("Step: 4")
+	samlSess := session.Must(session.NewSession())
+	svc := sts.New(samlSess)
+
+	samlParams := &sts.AssumeRoleWithSAMLInput{
+		PrincipalArn:    aws.String(principal),
+		RoleArn:         aws.String(role),
+		SAMLAssertion:   aws.String(assertion),
+		DurationSeconds: aws.Int64(int64(14400)),
+	}
+
+	samlResp, err := svc.AssumeRoleWithSAML(samlParams)
 	if err != nil {
+		log.WithField("role", role).Errorf(
+			"error assuming role with SAML: %s", err.Error())
 		return sts.Credentials{}, "", err
 	}
 
-	newCookieItem := keyring.Item{
-		Key:   "okta-session-cookie",
-		Data:  []byte(newSessionCookie),
-		Label: "okta session cookie",
-		KeychainNotTrustApplication: false,
-	}
-
-	p.Keyring.Set(newCookieItem)
-
-	return creds, oktaCreds.Username, err
+  return *samlResp.Credentials, oktaCreds.Username, nil
+	//return creds, oktaCreds.Username, err
 }
